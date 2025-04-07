a37bd5aaaf0792402634e58df8f250f939f934a9
hunghung200 <thuhahaihunglove@gmail.com>
2025-04-08 04:04:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const messages = [
+    "Bạn có chắc",
+    "Thật sự vậy",
+    "Bạn có suy nghĩ lại ko",
+    "Suy nghĩ đi làm ơn",
+    "Tôi nghĩ bạn ko nghĩ vậy đâu",
+    "Bạn từ chối tôi buồn lắm đó",
+    "Tôi rất buồn đó",
+    "Tôi rất rất bùn đó",
+    "Tôi nghĩ tôi lên dừng nói",
+    "Đồng ý đi làm ơn đó"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 2.0}px`;
+}
+
+function handleYesClick() {
+    // Ẩn trang Proposal và hiện trang Yes
+    document.getElementById("proposalPage").style.display = "none";
+    document.getElementById("yesPage").style.display = "block";
+}
